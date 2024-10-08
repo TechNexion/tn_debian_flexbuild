8d021665e9242cc357f9e8186c274e688f63141c
Joe Zhang <joe.zhang@technexion.com>
2024-10-08 15:46:32 +0800

    optee_os: add Technexion board support optee with evk binary

--- src/apps/security/optee_os.mk
+++ src/apps/security/optee_os.mk
@@ -38,6 +38,10 @@ ifeq ($(CONFIG_OPTEE),y)
 	elif [ $(SOCFAMILY) = IMX ]; then \
 		 if [ $(MACHINE) = qemuarm64 -o $(MACHINE) = all ]; then \
 		     brd=mx8mpevk; \
+		 elif [[ "${MACHINE}" == *imx8* ]] && [[ "${MACHINE}" != *evk* ]];then \
+		     brd=$$(echo $$(echo ${MACHINE}|grep -o "mx8..")evk); \
+		 elif [[ "${MACHINE}" == *imx9* ]] && [[ "${MACHINE}" != *evk* ]];then \
+		     brd=$$(echo $$(echo ${MACHINE}|grep -o "mx9.")evk); \
 		 else \
 		     brd=$${MACHINE:1}; \
 		 fi && \
