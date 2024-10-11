643aa30f8982667e9d019f410c8db9361596521b
Joe Zhang <joe.zhang@technexion.com>
2024-10-11 17:16:30 +0800

    imx8mp-edm-g: add initial support

--- src/bsp/imx_mkimage.mk
+++ src/bsp/imx_mkimage.mk
@@ -35,7 +35,7 @@ define imx_mkimage_target
 	bld mcore_demo; \
     fi && \
     \
-    if echo $1 | grep -qE ^imx8mp; then \
+    if echo $1 | grep -qE imx8mp; then \
 	SOC=iMX8MP; SOC_FAMILY=iMX8M; target=flash_evk; \
     elif echo $1 | grep -qE ^imx8mm; then \
 	SOC=iMX8MM; SOC_FAMILY=iMX8M; target=flash_evk; \
