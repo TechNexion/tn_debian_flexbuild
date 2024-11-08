02e0f652619bca4ac291c75b663539e0b9cd3ef8
Joe Zhang <joe.zhang@technexion.com>
2024-11-08 13:59:04 +0800

    tn_qca9377: add initial support

--- src/linux/linux.mk
+++ src/linux/linux.mk
@@ -58,7 +58,7 @@ linux:
 
 
 
-linux-modules: cryptodev_linux mdio_proxy_module isp_vvcam_module nxp_wlan_bt
+linux-modules: cryptodev_linux mdio_proxy_module isp_vvcam_module nxp_wlan_bt tn_qca9377
 	 $(call fbprint_d,"linux-modules")
 
 
