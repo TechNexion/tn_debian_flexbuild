1d112e40831d7eb1b1eecefc6b3b1c4ae25d6dba
Joe Zhang <joe.zhang@technexion.com>
2025-01-16 13:54:34 +0800

    apps: tn_bsp_services: resize_rfs: add log and reboot when done

--- src/apps/utils/tn_bsp_services/resize_rfs.sh
+++ src/apps/utils/tn_bsp_services/resize_rfs.sh
@@ -9,5 +9,19 @@ ROOT_PART_NAME=$(echo "$ROOT_DEVICE" | cut -d "/" -f 3)
 DEVICE_NAME=$(echo /sys/block/*/"${ROOT_PART_NAME}" | cut -d "/" -f 4)
 PART_ENTRY_NUMBER=$(cat "/sys/block/${DEVICE_NAME}/${ROOT_PART_NAME}/partition")
 
+sleep 5
+
+echo -e '\033[36m\n***********************************'
+echo 'Resizing storage at first boot.....'
+echo -e '***********************************\033[0m'
+
 sudo parted -s /dev/$DEVICE_NAME "resizepart ${PART_ENTRY_NUMBER} -1" quit
+sudo partprobe /dev/$DEVICE_NAME
+
 sudo resize2fs ${ROOT_DEVICE}
+
+sudo mount -o remount,rw /
+
+echo -e '\033[36m\n***********************************'
+echo 'Resizing task completes. Rebooting.....'
+echo -e '***********************************\033[0m'
