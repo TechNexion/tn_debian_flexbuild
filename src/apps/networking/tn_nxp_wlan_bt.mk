e248645bb9ab7101d2094f742b3883268adad42c
Tifa Chen <tifa.chen@technexion.com>
2026-01-20 17:36:15 +0800

    nxp_wlan_bt: add btnxpuart auto modprobe
    NXP Wifi/BT module use combo firmware file. BT driver should be
    probed after wifi driver.

--- src/apps/networking/tn_nxp_wlan_bt.mk
+++ src/apps/networking/tn_nxp_wlan_bt.mk
@@ -10,4 +10,6 @@ tn_nxp_wlan_bt:
 	 mkdir -p $(DESTDIR)/lib/modules-load.d && \
 	 echo "options moal mod_para=nxp/wifi_mod_para.conf" > $(DESTDIR)/lib/modprobe.d/moal.conf && \
 	 echo "moal" > $(DESTDIR)/lib/modules-load.d/moal.conf && \
+	 echo "btnxpuart" > $(DESTDIR)/lib/modules-load.d/btnxpuart.conf && \
+	 echo "softdep btnxpuart pre: moal" > $(DESTDIR)/lib/modprobe.d/bt_sequence.conf && \
 	 $(call fbprint_d,"tn_nxp_wlan_bt")
