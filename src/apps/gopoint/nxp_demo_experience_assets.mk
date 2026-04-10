770691db50885e28e14a6fc75d30f3000625bf8d
Tifa Chen <tifa.chen@technexion.com>
2026-04-10 18:15:54 +0800

    apps: gopoint: update NXP VIT model
    Update VIT model version for match VIT engine with 4.11.0 version.

--- src/apps/gopoint/nxp_demo_experience_assets.mk
+++ src/apps/gopoint/nxp_demo_experience_assets.mk
@@ -9,11 +9,16 @@
 #
 
 nxp_demo_experience_assets:
-ifeq ($(CONFIG_NXP_DEMO_EXPERIENCE_ASSETS),y)
+ifeq ($(CONFIG_NXP_DEMO_EXPERIENCE_ASSETS),"y")
 	@[ $(SOCFAMILY) != IMX ] && exit || \
 	 $(call fbprint_b,"nxp_demo_experience_assets") && \
 	 $(call download_repo,nxp_demo_experience_assets,apps/gopoint) && \
 	 $(call patch_apply,nxp_demo_experience_assets,apps/gopoint) && \
+	 cd $(GPDIR)/nxp_demo_experience_assets && \
+	 if [ ! -f .codedone ]; then \
+		 git clone https://oauth2:SbtQ_mC4fvJRA88_9jB7@gitlab.com/technexion-imx/nxp_vit_model.git; $(LOG_MUTE)\
+		 cp -Prf nxp_vit_model/* ./ && touch .codedone && rm -rf nxp_vit_model/; \
+	 fi && \
 	 \
 	 $(call fbprint_d,"nxp_demo_experience_assets")
 endif
